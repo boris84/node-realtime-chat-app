c9ec8a576a84f69c4c116a63796d2cef8fdda89d
boris84 <sutherland373@gmail.com>
2022-07-30 11:50:34 +0100

    added Heroku Data For Redis addon in Heroku

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ const {generateMessage, generateLocationMessage} = require('./utils/message');
 
 const redis = require('redis');
 const client = redis.createClient({
-  url: 'redis://127.0.0.1:6379'
+  url: 'redis://127.0.0.1:6379' 
 });
 client.connect().then(() => {
   console.log('redis client connected');
