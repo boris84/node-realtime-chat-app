33db803c378226620d58a20ac3f80acecd948585
boris84 <sutherland373@gmail.com>
2022-07-30 12:03:52 +0100

    configured REDIS_URL port

--- server/server.js
+++ server/server.js
@@ -19,8 +19,9 @@ const {generateMessage, generateLocationMessage} = require('./utils/message');
 
 
 const redis = require('redis');
+const url = process.env.REDIS_URL || 'redis://127.0.0.1:6379';
 const client = redis.createClient({
-  url: 'redis://127.0.0.1:6379' 
+  url: url
 });
 client.connect().then(() => {
   console.log('redis client connected');
